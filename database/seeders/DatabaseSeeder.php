b432b5d1481a104215a46920e42b5e4dbe5251b8
Ajikgungz <gungdemahindra0629@gmail.com git config --global user.name>
2026-09-08 15:21:08 +0800

    Remove InternshipSeeder to avoid merge conflicts

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -38,7 +38,6 @@ class DatabaseSeeder extends Seeder
             JobListingSeeder::class,
             SchoolSeeder::class,
             CompanySeeder::class,
-            InternshipSeeder::class,
         ]);
     }
 }
